d286d78bccb8c45bcb85df929b68356daf8337b4
Techie-Mahesh <maheshTech1205@gmail.com>
2025-01-25 15:58:14 +0530

    eslint configuration updated

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": "error",
+      "no-unused-vars": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true }
